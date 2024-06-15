e5e737181bb31e35e35d23d92beb26382123d23a
mengmx01 <mengxiang2001@gmail.com>
2024-06-16 00:33:36 +1000

    投票记录和提名阶段加入座位号

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -6,8 +6,8 @@
     </div>
     <div class="overlay">
       <audio src="../assets/sounds/countdown.mp3" preload="auto"></audio>
-      <em class="blue">{{ nominator.name }}</em> 提名了
-      <em>{{ nominee.name }}</em
+      <em class="blue">{{ session.nomination[0] + ". " + nominator.name }}</em> 提名了
+      <em>{{ session.nomination[1] + ". " + nominee.name }}</em
       >！
       <br />
       <em class="blue">
